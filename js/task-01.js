32cb3b7a5d82c53c1dce23c09c5aa13c93c1206e
i88sharova <i88sharova@gmail.com>
2023-12-11 22:18:36 +0100

    DZ8-1

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const liEls = document.querySelectorAll(".item");
-console.log(`Number of categories: ${liEls.length}`);
-
-liEls.forEach((element) => {
-  const titleOfcategory = element.firstElementChild.textContent;
-  const elsOfcategory = element.lastElementChild.children;
-  console.log(`Category: ${titleOfcategory}`);
-  console.log(`Elements: ${elsOfcategory.length}`);
-});
--- js/task-03.js
+++
@@ -1,6 +0,0 @@
-const nameOutput=document.querySelector("#name-output");
-const nameInput = document.querySelector("#name-input");
-nameInput.addEventListener('input', (event) => {
-    const inputValue = event.target.value.trim();
-    nameOutput.textContent = inputValue?inputValue:'Anonymous';
-  });
